19b6af9717da2cff2b720facc3125d776b3c90ee
Matthew Iadanza <fbsmi@absl-wkstn02.leeds.ac.uk>
2019-02-07 08:20:12 +0000

    made line thicker

--- sum_gaussians.py
+++ sum_gaussians.py
@@ -39,6 +39,6 @@ for i in zip(means,stds,pcts):
     tstd = i[1]
     pct = i[2]
     x_values = np.linspace(tmean-(4*tstd), tmean+(4*tstd), 120)
-    plt.plot(x_values, multigaussian(x_values, zip(means,stds,pcts)))
+    plt.plot(x_values, multigaussian(x_values, zip(means,stds,pcts)),linewidth=4.0)
     
 plt.savefig('gaussians.png')
