1d8c02b999e43ed290626a4d729e56c03f844578
Matthew Iadanza <fbsmi@absl-wkstn02.leeds.ac.uk>
2019-02-07 07:48:28 +0000

    initial

---
+++ sum_gaussians.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
+import math
+
+means = [float(i) for i in sys.argv[1::3]]
+stds = [float(i) for i in sys.argv[2::3]]
+pcts = [float(i) for i in sys.argv[3::3]]
+
+def gaussian(x, mu, sig,pct):
+    return pct*(np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))))
+
+for i in zip(means,stds,pcts):
+    tmean = i[0]
+    tstd = i[1]
+    pct = i[2]
+    x_values = np.linspace(tmean-(4*tstd), tmean+(4*tstd), 120)
+    plt.plot(x_values, gaussian(x_values, tmean, tstd,pct))
+
+def multigaussian(x, array):
+    thesum = 0
+    for i in array:
+        pct = i[2]
+        mu = i[0]
+        sig = i[1]
+        val = pct*(np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))))
+        thesum= thesum+val
+    return(thesum)
+
+for i in zip(means,stds,pcts):
+    tmean = i[0]
+    tstd = i[1]
+    pct = i[2]
+    x_values = np.linspace(tmean-(4*tstd), tmean+(4*tstd), 120)
+    plt.plot(x_values, multigaussian(x_values, zip(means,stds,pcts)))
+    
+plt.savefig('gaussians.png')
